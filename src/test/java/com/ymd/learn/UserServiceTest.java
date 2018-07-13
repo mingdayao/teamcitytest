63ca40dda64ba882f6351626f054ad90e19e4c68
Michael Yao <Michael_Yao@epam.com>
2018-07-13 12:09:46 +0800

    commit code

---
+++ src/test/java/com/ymd/learn/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.ymd.learn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserServiceTest {
+	
+	@Test
+	public void testGetUserById() {
+		Assert.assertFalse(false);
+	}
+}
